e9952edbd14217f6a78e2b6dead86a3a1647a1ac
krazytristan <trstnjorge@gmail.com>
2025-08-10 02:49:57 +0800

    new update

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./index.html","./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class",
-  theme: {
-    extend: {
-      fontFamily: {
-        inter: ["Inter", "ui-sans-serif", "system-ui", "sans-serif"],
-      },
-    },
-  },
+  theme: { extend: {} },
   plugins: [],
 }
